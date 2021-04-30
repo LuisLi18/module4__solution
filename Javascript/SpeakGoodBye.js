8b6076ed0c218d62051f952c8c4095d787105a33
LuisLi18 <81203064+LuisLi18@users.noreply.github.com>
2021-04-30 01:10:10 -0500

    Delete Javascript directory

--- Javascript/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    byeSpeaker = new Object();
-    //byeSpeaker.name = "Carolina";
-    var speakWord = "Good Bye";
-    byeSpeaker.speak = function(names) {
-        console.log(speakWord + " " + names);
-      }
-      window.byeSpeaker = byeSpeaker;
-})(names, window);
