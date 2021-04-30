5035be9f8697c817d50d93f172140d502ca71cc5
LuisLi18 <81203064+LuisLi18@users.noreply.github.com>
2021-04-30 01:07:13 -0500

    Delete SpeakHello.js

--- SpeakHello.js
+++
@@ -1,8 +0,0 @@
-  (function (){
-    var helloSpeaker = {};
-    var speakWord = "Hello";
-    helloSpeaker.speak = function(names){
-            console.log(speakWord + " " + names);
-    }
-    window.helloSpeaker = helloSpeaker;
-})(names, window);
